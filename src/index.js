d3fd61b33019f7a6a2c090713af43ffad3b58d77
Shubham Bisht <bisht.shubham.emp@gmail.com>
2024-03-14 23:45:34 +0530

    Creating utility ApiError and ApiResponse classes

--- src/index.js
+++ src/index.js
@@ -1,8 +1,14 @@
-// require('dotenv').config({path:'./env'})
+import { app } from "./app.js";
 import dotenv from "dotenv";
 import connectDB from "./db/index.js";
 dotenv.config({
   path: "./env",
 });
 
-connectDB();
+connectDB()
+  .then(() => {
+    app.listen(process.env.PORT || 8000, () =>
+      console.log(`Server running at PORT ${process.env.PORT}`)
+    );
+  })
+  .catch((error) => console.log("MONGO db connection failed!!!", error));
