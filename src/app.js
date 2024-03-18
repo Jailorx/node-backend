92925dffd313b8bfbaf35a8ba0e480126aaa52bc
Shubham Bisht <bisht.shubham.emp@gmail.com>
2024-03-18 20:22:54 +0530

    all setup configured

--- src/app.js
+++ src/app.js
@@ -10,13 +10,14 @@ app.use(
     credentials: true,
   })
 );
-
 app.use(express.json({ limit: "16kb" }));
-
-app.use(express.urlencoded({ extendedLtrue, limit: "16kb" }));
-
+app.use(express.urlencoded({ extended: true, limit: "16kb" }));
 app.use(express.static("public"));
-
 app.use(cookieParser());
+
+//import routes
+import userRouter from "./routes/user.routes.js";
+
+app.use("/api/v1/users", userRouter);
 
 export { app };
